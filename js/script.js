f3f7f40973faf5a64048726c18562906a48e225e
SeasonedMiso <zakwrn@gmail.com>
2020-12-10 16:07:51 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let input = document.getElementById("FILE");
+
+// This event listener has been implemented to identify a
+// Change in the input section of the html code
+// It will be triggered when a file is chosen.
+input.addEventListener("change", () => {
+  let files = input.files;
+  if (files.length == 0) return;
+
+  /* If any further modifications have to be made on the 
+       Extracted text. The text can be accessed using the  
+       file variable. But since this is const, it is a read  
+       only variable, hence immutable. To make any changes,  
+       changing const to var, here and In the reader.onload  
+       function would be advisible */
+  const file = files[0];
+
+  let reader = new FileReader();
+
+  reader.onload = (e) => {
+    console.log(event.target.result);
+    const file = e.target.result;
+
+    // This is a regular expression to identify carriage
+    // Returns and line breaks
+    const lines = file.split(/\r\n|\n/);
+  };
+
+  reader.onerror = (e) => alert(e.target.error.name);
+
+  reader.readAsText(file);
+});
